633b066e85bb256fed213e69f3aa39f781ca1024
crackoscl <gilbert.ata@gmail.com>
2020-12-01 23:02:00 -0300

    quitar archivos que estan demas

--- calculoscientifico3/nuevos_calculos2.py
+++
@@ -1,9 +0,0 @@
-from packcientifico.funciones import pi
-if __name__ == "__main__":
-    array = [0,1,2,3,4,5,6,7,8,9,10]
-    t = array
-    x = (10)
-    if x == -1:
-        print ("not found")
-    else:
-        print ("found at"), x
