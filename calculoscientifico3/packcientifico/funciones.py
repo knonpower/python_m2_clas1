8faa0b47acf481fff7a42ae50d6cc7aa6ba72a9d
Pamms <pmartinezsierra@csu.fullerton.edu>
2020-12-06 15:53:13 -0800

    modifica funciones y agrego calculos fisica y finanzas

--- calculoscientifico3/packcientifico/funciones.py
+++ calculoscientifico3/packcientifico/funciones.py
@@ -30,3 +30,29 @@ def perimetro_rectangulo(base, altura):
 def distancia_recorrida(t,v):
     d_recorrida = t / v
     return d_recorrida    
+
+    #Calculos Fisica
+def Velocidad_Promedio(Vi, Vf):
+    V_promedio = Vi + Vf / 2
+    return V_promedio
+
+def Aceleracion(Vi, Vf, t):
+    Aceleracion = Vi - Vf / t
+    return Aceleracion
+
+def Movimiento_Uniforme (Vf, t):
+    M_uniforme = Vf / t
+    return M_uniforme
+
+        #Calculos Finanzas
+def Interes_simple (P, i, T):
+    I_simple = P * i * T
+    return I_simple
+
+def Valor_con_vencimiento_Futuro(P, I):
+    V_vencimientof = P + I
+    return V_vencimientof
+
+def Capital_Principal (S, I):
+    C_principal = S - I
+    return C_principal
